0f43c4b714a3394fea111554688118e68e05e551
abey0 <rabeywickrem@student.unimelb.edu.au>
2025-03-09 23:07:02 +1100

    Main game file made, lab scenes + drone_level in

---
+++ main_game/Drone_Level/button.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+
+signal hit
+var x = 680
+var y = 400
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+
+func start():
+	$Sprite2D.visible = false
+	$Sprite2D2.visible = false
+	$CollisionShape2D.disabled = true
+	position.x = x
+	position.y = y
+	
+func appear():
+	$Sprite2D.visible = true
+
+	$CollisionShape2D.disabled = false
+	
+	
+func _on_body_entered(body):
+	hit.emit()
+	$Sprite2D2.visible = true
+	$Sprite2D.visible = false
+	
---
+++ main_game/Lab_Room_2/beacon.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Off.show()
+	$On.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func activate() -> void:
+	$Off.hide()
+	$On.show()
